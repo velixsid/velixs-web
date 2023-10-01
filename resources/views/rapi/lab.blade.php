61ef9821ce4f1092af7dc2df1f38c4d263b19118
ilsyaa <ilham.shindou@gmail.com>
2023-10-01 15:56:57 +0700

    rename header data to body

--- resources/views/rapi/lab.blade.php
+++ resources/views/rapi/lab.blade.php
@@ -102,7 +102,7 @@
                                             <div class="p-[5px] bg-green-600 inline-block rounded-full"></div>
                                         </div>
                                         <div class="text-xs text-slate-700 dark:text-slate-300 flex items-center gap-1">
-                                            data : { }
+                                            body : { }
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                                 <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path>
